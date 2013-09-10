a79c9bff75e3c75195bf9ba1494340588ad8a6f9
gaopeng <gaop-a@grandsoft.com.cn>
2013-09-10 13:56:29 +0800

    add files

---
+++ taobaoztc/src/main/java/com/fnpnet/taobao/taobaoztc/filter/filter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fnpnet.taobao.taobaoztc.filter;
+
+/**
+ *
+ * @author pc
+ */
+public class filter {
+    
+}
---
+++ taobaoztc/src/main/java/com/fnpnet/taobao/taobaoztc/util/test.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fnpnet.taobao.taobaoztc.util;
+/**
+ *
+ * @author pc
+ */
+public class test {
+    
+}
